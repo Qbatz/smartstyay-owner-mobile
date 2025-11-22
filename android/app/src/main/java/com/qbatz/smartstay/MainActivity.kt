5e8816f923a6ab85b3387281152842b6592834fa
Allwin <allwinpon@gmail.com>
2025-11-22 13:21:30 +0530

    package name changes

--- android/app/src/main/java/com/qbatz/smartstay/MainActivity.kt
+++ android/app/src/main/java/com/qbatz/smartstay/MainActivity.kt
@@ -1,4 +1,5 @@
-package com.smartstayownerapp
+package com.qbatz.smartstay;
+
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/qbatz/smartstay/MainApplication.kt
+++ android/app/src/main/java/com/qbatz/smartstay/MainApplication.kt
@@ -1,4 +1,5 @@
-package com.smartstayownerapp
+package com.qbatz.smartstay;
+
 
 import android.app.Application
 import com.facebook.react.PackageList
