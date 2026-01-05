083356f1a243093b33833efbfe7b0f3e38e4cd18
Sivasri234 <priyarajeshsri143@gmail.com>
2026-01-06 00:29:43 +0530

    change

--- App.jsx
+++ App.jsx
@@ -219,10 +219,7 @@ function AppContent(props) {
           <Navigation.Screen name="CreateAccount" component={CreateAccount} />
           <Navigation.Screen name="LoginDesign" component={LoginDesign} />
           <Navigation.Screen name="CreateMpin" component={CreateMpin} />
-          <Navigation.Screen name="ConfirmMPin" component={ConfirmMPin} />
-          <Navigation.Screen name="EnterMPin" component={EnterMPin} />
-            <Navigation.Screen name="MyTabs" component={MyTabs} />
-          
+         
         </Navigation.Navigator>
         </NavigationContainer>}
 
