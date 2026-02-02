1293ede9e506c5684224c269ebf20df429428f74
Sivasri234 <priyarajeshsri143@gmail.com>
2026-02-02 21:09:42 +0530

    compliane_empty

--- src/Component/Customer/CustomerOverview/ComplaintsTab.jsx
+++ src/Component/Customer/CustomerOverview/ComplaintsTab.jsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import EmptyState from "../../../Assets/Images/Empty_state.png";
 
 const ComplaintsTab = () => {
   return (
-    <View style={{ padding: 20 }}>
-      <Text>Compliance Screen</Text>
+    <View style={styles.container}>
+      <Image source={EmptyState} style={styles.image} resizeMode="contain" />
+      <Text style={styles.text}>No Complaints Found</Text>
     </View>
   );
 };
 
 export default ComplaintsTab;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  image: {
+    width: 180,
+    height: 180,
+  },
+
+  text: {
+    marginTop: 15,
+    fontSize: 16,
+    color: "#6B7280",
+  },
+});
+
